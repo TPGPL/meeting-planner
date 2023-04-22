7b7cb62fbc3464192f7f04a9e09ccb6ca8407b3e
TPGPL <mateuszkwiatkowski2002@gmail.com>
2023-04-22 14:44:26 +0200

    Simplify break removal condition

--- src/main/java/tpgpl/calendar/MeetingPlanner.java
+++ src/main/java/tpgpl/calendar/MeetingPlanner.java
@@ -21,18 +21,10 @@ public class MeetingPlanner {
         List<TimeRange> firstBreakTimes = firstCalendar.getBreakTimes();
         List<TimeRange> secondBreakTimes = secondCalendar.getBreakTimes();
 
-        // 2. Remove breaks shorter than meeting duration and not within possible working hours
+        // 2. Remove breaks shorter than meeting duration
 
-        Timestamp higherStart = firstCalendar.getWorkingHours().start().compareTo(secondCalendar.getWorkingHours().start()) >= 0
-                ? firstCalendar.getWorkingHours().start() : secondCalendar.getWorkingHours().start();
-
-        Timestamp lowerEnd = firstCalendar.getWorkingHours().end().compareTo(secondCalendar.getWorkingHours().end()) <= 0
-                ? firstCalendar.getWorkingHours().end() : secondCalendar.getWorkingHours().end();
-
-        TimeRange validMeetingRange = new TimeRange(higherStart, lowerEnd);
-
-        firstBreakTimes.removeIf(m -> m.getDurationInMinutes() < meetingDuration || !validMeetingRange.containsRange(m));
-        secondBreakTimes.removeIf(m -> m.getDurationInMinutes() < meetingDuration || !validMeetingRange.containsRange(m));
+        firstBreakTimes.removeIf(m -> m.getDurationInMinutes() < meetingDuration);
+        secondBreakTimes.removeIf(m -> m.getDurationInMinutes() < meetingDuration);
 
         // 3. Calculate possible meeting times for colliding breaks
 
